ae6c9a1c46ef6fe84af0ad7780ca405cd252f1f3
Firelock <javier.huaylla@gmail.com>
2019-02-11 23:52:38 -0300

    DB/Quest: Update: The Way of the Tushui (29414)

---
+++ sql/updates/world/2019_02_11_01_world_quest_template.sql
@@ -0,0 +1,4 @@
+-- DB/Quest: Update: The Way of the Tushui (29414)
+-- [Map: The Wandering Isle, Area: Fu's Pond, Zone: The Wandering Isle]
+
+UPDATE `quest_template` SET `RewardChoiceItemId1`=73224, `RewardChoiceItemId2`=73225, `RewardChoiceItemId3`=73226, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1,`RewardChoiceItemCount3`=1 WHERE `id`=29414;
