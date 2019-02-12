756df76d381159e45ad651907368f3a71c95d67b
Firelock <javier.huaylla@gmail.com>
2019-02-12 01:31:39 -0300

    DB/Quest: Update: The Lesson of Dry Fur (29661)

---
+++ sql/updates/world/2019_02_12_03_world_quest_template.sql
@@ -0,0 +1,3 @@
+-- DB/Quest: Update: The Lesson of Dry Fur (29661)
+
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74568, `RewardChoiceItemId2`=74569, `RewardChoiceItemId3`=74570, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29661;
