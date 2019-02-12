cbd8553b0f032502310d67175f2b26a5a17b9a10
Firelock <javier.huaylla@gmail.com>
2019-02-12 01:17:23 -0300

    DB/Quest: Update: The Passion of Shen-zin Su (29423)

---
+++ sql/updates/world/2019_02_12_02_world_quest_template.sql
@@ -0,0 +1,3 @@
+-- DB/Quest: Update: The Passion of Shen-zin Su (29423)
+
+UPDATE `quest_template` SET `RewardChoiceItemId1`=73236, `RewardChoiceItemId2`=73234, `RewardChoiceItemId3`=73233, `RewardChoiceItemId4`=73235, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1, `RewardChoiceItemCount4`=1 WHERE `id`=29423;
