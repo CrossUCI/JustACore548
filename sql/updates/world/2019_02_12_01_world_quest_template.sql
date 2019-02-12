f5f0f9a7e7727b1e583cf74acb69caddd8b41da7
Firelock <javier.huaylla@gmail.com>
2019-02-12 00:35:28 -0300

    DB/Quest: Update: Fanning the Flames (29523)

---
+++ sql/updates/world/2019_02_12_01_world_quest_template.sql
@@ -0,0 +1,3 @@
+-- DB/Quest: Update: Fanning the Flames (29523)
+
+UPDATE `quest_template` SET `RewardChoiceItemId1`=73230, `RewardChoiceItemId2`=73231, `RewardChoiceItemId3`=73232, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1,`RewardChoiceItemCount3`=1 WHERE `id`=29523;
