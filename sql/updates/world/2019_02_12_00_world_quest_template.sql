e44db2882fe419bd691cb2bdf27a92b9bc7ce90a
Firelock <javier.huaylla@gmail.com>
2019-02-12 00:14:08 -0300

    DB/Quest: Update: The Way of the Huojin (29417)

---
+++ sql/updates/world/2019_02_12_00_world_quest_template.sql
@@ -0,0 +1,3 @@
+-- DB/Quest: Update: The Way of the Huojin (29417)
+
+UPDATE `quest_template` SET `RewardChoiceItemId1`=73227, `RewardChoiceItemId2`=73228, `RewardChoiceItemId3`=73229, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1,`RewardChoiceItemCount3`=1 WHERE `id`=29417;
