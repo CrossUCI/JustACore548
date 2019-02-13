ad2273a66872e933632189ce39324b6003aaaac5
Firelock <javier.huaylla@gmail.com>
2019-02-13 01:39:29 -0300

    DB/Quest: Agregado Rewards faltantes a las misiones iniciales de los pandas.

---
+++ sql/updates/world/2019_02_13_00_world_quest_template.sql
@@ -0,0 +1,26 @@
+-- DB/Quest: Agregado Rewards faltantes a las misiones iniciales de los pandas.
+
+-- The Sun Pearl(29677)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74574, `RewardChoiceItemId2`=74575, `RewardChoiceItemId3`=74576, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29677;
+-- A New Friend (29679)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74571, `RewardChoiceItemId2`=74572, `RewardChoiceItemId3`=74573, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29679;
+-- Still Good! (29770)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74580, `RewardChoiceItemId2`=74581, `RewardChoiceItemId3`=74582, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29770;
+-- Missing Mallet (29768)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74583, `RewardChoiceItemId2`=74584, `RewardChoiceItemId3`=74585, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29768;
+-- The Spirit and Body of Shen-zin Su (29775)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74577, `RewardChoiceItemId2`=74578, `RewardChoiceItemId3`=74579, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29775;
+-- Rewritten Wisdoms (29778)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74594, `RewardChoiceItemId2`=74595, `RewardChoiceItemId3`=74596, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29778;
+-- Do No Evil (29780)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74592, `RewardChoiceItemId2`=74593, `RewardChoiceItemId3`=74591, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29780;
+-- Stronger Than Bone (29782)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74590, `RewardChoiceItemId2`=74588, `RewardChoiceItemId3`=74587, `RewardChoiceItemId4`=74589, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29782;
+-- Passing Wisdom (29790)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74597, `RewardChoiceItemId2`=74598, `RewardChoiceItemId3`=74599, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29790;
+-- Evil from the Seas (29793)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74606, `RewardChoiceItemId2`=74607, `RewardChoiceItemId3`=74608, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29793;
+-- None Left Behind (29794)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=74603, `RewardChoiceItemId2`=74604, `RewardChoiceItemId3`=74605, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29794;
+-- The Healing of Shen-zin Su (29799)
+UPDATE `quest_template` SET `RewardChoiceItemId1`=75600, `RewardChoiceItemId2`=75601, `RewardChoiceItemId3`=75602, `RewardChoiceItemCount1`=1, `RewardChoiceItemCount2`=1, `RewardChoiceItemCount3`=1 WHERE `id`=29799;
